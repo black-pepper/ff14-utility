b6307ca3dfb4e34de116d69f0fec61410196799b
Uju <56465463+black-pepper@users.noreply.github.com>
2025-07-27 22:51:38 +0900

    add event

--- src/config/eventList.js
+++ src/config/eventList.js
@@ -1,30 +1,23 @@
 export default {
     eventList: [
         {
+            title: "신규 업데이트와 함께하는 낭만의 모험",
+            startDate: new Date('2025-07-15T12:00:00'),
+            endDate: new Date('2025-08-18'),
+            ReceivingDate: new Date('2025-09-15'),
+            url: "https://www.ff14.co.kr/events/2025/v7_2_promotion_all_a",
+        },
+        {
+            title: "불꽃축제",
+            startDate: new Date('2025-07-15T12:00:00'),
+            endDate: new Date('2025-07-28'),
+            url: "https://www.ff14.co.kr/events/fun/2025/firefestival",
+        },
+        {
             title: "모그모그★컬렉션 ~환상과의 해후~",
             startDate: new Date('2025-06-17T12:00:00'),
             endDate: new Date('2025-07-15'),
             url: "https://www.ff14.co.kr/events/fun/mogmog-collection/71x_part1"
-        },
-        {
-            title: "'멸 어둠의 구름 격투전'자격증 취득 프로젝트",
-            startDate: new Date('2025-05-27T12:00:00'),
-            endDate: new Date('2025-06-23'),
-            ReceivingDate: new Date('2025-07-21'),
-            url: "https://www.ff14.co.kr/events/2025/A0527"
-        },
-        {
-            title: "치지직 드롭스 이벤트",
-            startDate: new Date('2025-05-27'),
-            endDate: new Date('2025-06-23'),
-            url: "https://www.ff14.co.kr/news/event/view/654"
-        },
-        {
-            title: "'또 하나의 미래' 이벤트",
-            startDate: new Date('2025-04-15T12:00:00'),
-            endDate: new Date('2025-05-12'),
-            ReceivingDate: new Date('2025-07-07'),
-            url: "https://www.ff14.co.kr/events/2025/FuturesRewritten"
         },
         {
             title: "파이널판타지14 x 처갓집 양념 치킨",
